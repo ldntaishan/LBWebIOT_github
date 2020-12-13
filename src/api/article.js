20a8bdea1a2493c573d93cfe43a5e02128dab94f
ldn <lzw_weiwei@163.com>
2020-12-13 19:32:26 +0800

    更新 202-12-13

--- src/api/article.js
+++ src/api/article.js
@@ -1,5 +1,9 @@
 import request from '@/utils/request'
 
+const serverBaseURL = 'http://localhost:8080/LBServerIOT_war_exploded'// 我的笔记本环境
+// const serverBaseURL = "http://localhost:8080/LBServerIOT_github_war";//我的台式机环境
+// const serverBaseURL = "http://39.106.55.120:8080";//线上服务器环境
+
 export function fetchList(query) {
   return request({
     url: '/vue-element-admin/article/list',
@@ -10,18 +14,17 @@ export function fetchList(query) {
 
 export function seAllUserList(query) {
   return request({
-    // url: 'http://39.106.55.120:8080/app/seAllUserList',
-    url: 'http://localhost:8080/LBServerIOT_github_war/app/seAllUserList',
+    url: serverBaseURL + '/app/seAllUserList',
     method: 'get',
-    params: {query}
+    params: { query }
   })
 }
 
-export function createUser(userName,userNickName,userEmail,userTel,userRole,userPassWord) {
+export function createUser(userName, userNickName, userEmail, userTel, userRole, userPassWord) {
   return request({
-    url: 'http://localhost:8080/LBServerIOT_github_war/app/userSignin',
+    url: serverBaseURL + '/app/userSignin',
     method: 'get',
-    params: { userName,userNickName,userEmail,userTel,userRole,userPassWord }
+    params: { userName, userNickName, userEmail, userTel, userRole, userPassWord }
   })
 }
 
@@ -34,12 +37,11 @@ export function createUser(userName,userNickName,userEmail,userTel,userRole,user
 // }
 export function userDeleteObj(userBaseId) {
   return request({
-    url: 'http://localhost:8080/LBServerIOT_github_war/app/deluser',
+    url: serverBaseURL + '/app/deluser',
     method: 'get',
     params: { userBaseId }
   })
 }
-
 
 export function fetchArticle(id) {
   return request({
