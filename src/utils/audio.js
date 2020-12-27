956a9eabd9eaff96594e91d22900717cb67dbf40
ldn <lzw_weiwei@163.com>
2020-12-28 00:38:25 +0800

    版本号：0.1 修改日期：20201228 说明：常规开发更新

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -1,4 +1,4 @@
 export function playAlarmSound() {
-  const gamemuiscs1 = new Audio(require('@/assets/music/alert_woman.wav.wav'));
+  const gamemuiscs1 = new Audio(require('@/assets/music/alert_woman.wav'));
   gamemuiscs1.play();
 }
